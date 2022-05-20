9f58774a17171dedfb097e97da900bb30fcb021d
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-05-20 15:28:07 +0530

    changed the contract of head to accept seperator

--- src/headLib.js
+++ src/headLib.js
@@ -1,11 +1,12 @@
-const separateLines = (content) => content.split('\n');
-const joinLines = (lines) => lines.join('\n');
+const separateLines = (content, separator) => content.split(separator);
+const joinLines = (lines, separator) => lines.join(separator);
 
 const firstNLines = (lines, lineCount) => lines.slice(0, lineCount);
 
-const head = function (content, lineCount) {
-  const lines = separateLines(content);
-  return joinLines(firstNLines(lines, lineCount));
-};  
+const head = function (content, lineCount, separator) {
+  const lines = separateLines(content, separator);
+  return joinLines(firstNLines(lines, lineCount), separator);
+};
+
 exports.head = head;
 exports.firstNLines = firstNLines;
