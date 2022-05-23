2a23cd321edcb35800fbe739df95c6c8fe117db3
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-05-23 10:58:35 +0530

    removed string manipulatejs

--- src/stringManipulate.js
+++
@@ -1,3 +0,0 @@
-exports.separateComponents = (content, separator) => content.split(separator);
-exports.joinComponents = (lines, separator) => lines.join(separator);
-exports.firstNItems = (lines, itemCount) => lines.slice(0, itemCount);
